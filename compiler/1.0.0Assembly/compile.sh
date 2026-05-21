bf359638a05a3547fed121c4c3b74e2eef5f251f
C-r0 <146779565+C-r0@users.noreply.github.com>
2026-05-21 14:35:53 +0000

    Delete compiler/1.0.0Assembly directory

--- compiler/1.0.0Assembly/compile.sh
+++
@@ -1,7 +0,0 @@
-nasm -f elf64 src/compiler.asm -o build/compiler.o
-ld build/compiler.o -o build/compiler
-./build/compiler $1 $2
-nasm -f elf64 src/loader.asm -o build/loader.o
-ld build/loader.o -o $3
-echo "----------------------------"
-./$3
